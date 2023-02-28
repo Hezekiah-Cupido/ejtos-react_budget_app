c65124a8417d26325da7390b26d29152fa5231c1
hezekiah <hezekiahcupido@gmail.com>
2023-02-28 17:04:16 +0200

    initial

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,27 @@
+import React, {useContext} from "react";
+import ExpanseItem from "./ExpenseItem";
+import { AppContext } from "../context/AppContext";
 
+const ExpenseList = () => {
+    let {expenses} = useContext(AppContext);
+
+    return (
+        <table className="table">
+            <thead className="thead-light">
+                <tr>
+                    <th scope="col">Department</th>
+                    <th scope="col">Allocated Budget</th>
+                    <th scope="col">Increase by 10</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map((expense) => 
+                    <ExpanseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
+                )}
+            </tbody>
+        </table>
+    );
+};
+
+export default ExpenseList;
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,16 @@
+import React, {useContext} from "react";
+import { AppContext } from "../context/AppContext";
 
+const ExpenseTotal = () => {
+    let {expenses} = useContext(AppContext);
+
+    let totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+
+    return (
+        <div className="alert alert-primary">
+            <span>Spent so far: £{totalExpenses}</span>
+        </div>
+    );
+};
+
+export default ExpenseTotal;
